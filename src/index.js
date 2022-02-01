d4bea5cb565f537b0b7e596aaa4ce690cb1c6fb9
Dzmitry Nikalayeu <dimitra86@list.ru>
2022-02-01 22:10:31 +0300

    feat: finish kata

--- src/index.js
+++ src/index.js
@@ -35,10 +35,48 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**':  ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+    const length = 10; // длина одной части
+    const pattern = new RegExp(".{1," + length + "}", "ig");
+    let res = expr.match(pattern).map(item => item.padEnd(length, "."));
+    console.log(res);
+    let lett = [];
+    for(let j=0;j<res.length;j++){
+    let str = res[j].toString();
+    console.log(str);
+    let strM = '';
+    
+    for (let i = 0; i < str.length; i++) {
+      if (i % 2 == 0) {
+    
+        if (str[i] + str[i + 1] == 10) {
+          strM += `.`;
+         // console.log(i);
+        }
+        else if (str[i] + str[i + 1] == 11) {
+          strM += `-`;
+         // console.log(i);
+        }
+        else if (str[i] + str[i + 1] == '**') {
+          strM += '**';
+          break;
+        }
+      }
+    }
+    //console.log(strM);
+    
+    
+    for (let key in MORSE_TABLE) {
+      // ключи
+      if (key == strM) { lett.push(MORSE_TABLE[key]); }
+      // значения ключей
+    }
+    
+    }
+   return lett.join('');  // write your solution here
 }
 
 module.exports = {
